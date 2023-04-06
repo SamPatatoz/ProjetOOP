94bb37bda41a0046f27fc001b5e17506baf52cbb
SamPatatoz <issamissam@hotmail.be>
2023-04-06 19:39:25 +0200

    Check the input argument in main

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
 public class Game {
     public static void main(final String[] args) {
-
+        //Check the input argument
+        if(!(args.length > 0))
+            System.out.println("ERROR : missing argument (Input .txt file)");
     }
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,10 @@
 public class Node {
-    
+    private String data;
+    private Node left;
+    private Node right;
+
+    public Node(int value){
+        right = null;
+        left = null;
+    }
 }
